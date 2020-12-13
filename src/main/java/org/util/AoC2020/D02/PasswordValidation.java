842fbc6d9e9f05ea13a743e74c801fc8794d790b
Bastien Gallay <bgallay@palo-it.com>
2020-12-13 19:32:20 +0100

    Day 2 - All tests done

--- src/main/java/org/util/AoC2020/D02/PasswordValidation.java
+++ src/main/java/org/util/AoC2020/D02/PasswordValidation.java
@@ -1,9 +1,21 @@
 package org.util.AoC2020.D02;
 
+import org.javatuples.Pair;
+
+import java.util.List;
+
 public class PasswordValidation {
     public static boolean validate(PasswordPolicy passwordPolicy, String password) {
         final long countRequiredCharacter = password.chars().filter((c) -> c == passwordPolicy.getCharacter()).count();
-        return countRequiredCharacter <= passwordPolicy.getRangeMin()
-                && countRequiredCharacter >= passwordPolicy.getRangeMax();
+        return countRequiredCharacter >= passwordPolicy.getRangeMin()
+                && countRequiredCharacter <= passwordPolicy.getRangeMax();
+    }
+
+    public static long countFromList(List<Pair<PasswordPolicy, String>> passwordsToCheck) {
+        return passwordsToCheck.stream().filter(PasswordValidation::validate).count();
+    }
+
+    private static boolean validate(Pair<PasswordPolicy, String> passwordPolicyStringPair) {
+        return validate(passwordPolicyStringPair.getValue0(), passwordPolicyStringPair.getValue1());
     }
 }
--- src/test/java/D02/PasswordValidationTest.java
+++ src/test/java/D02/PasswordValidationTest.java
@@ -1,19 +1,19 @@
 package D02;
 
 import D02.configurator.ValidPasswordPolicy;
-import net.jqwik.api.*;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.apache.commons.lang3.StringUtils;
+import org.javatuples.Pair;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.util.AoC2020.D02.PasswordPolicy;
 import org.util.AoC2020.D02.PasswordValidation;
+import org.util.AoC2020.Helpers;
+
+import java.util.List;
 
 public class PasswordValidationTest {
-//    List<Pair<PasswordPolicy, String>> actual = Helpers.getConvertedPolicyAndPasswordInputs(
-//            "d02-tests.txt",
-//            Helpers::passwordPolicyAndStringFromEntry
-//    );
-
     @Test
     void PasswordPolicyApplies() {
         final PasswordPolicy passwordPolicy = new PasswordPolicy(1, 3, 'a');
@@ -52,5 +52,15 @@ public class PasswordValidationTest {
     ) {
         String password = StringUtils.repeat(passwordPolicy.getCharacter(), passwordPolicy.getRangeMin() - 1);
         Assert.assertFalse(PasswordValidation.validate(passwordPolicy, password));
+    }
+
+    @Test
+    void ThereAreExactly2ValidPasswordsInThisList() {
+        final long expected = 2;
+        List<Pair<PasswordPolicy, String>> passwordsToCheck = Helpers.getConvertedPolicyAndPasswordInputs(
+                "d02-tests.txt",
+                Helpers::passwordPolicyAndStringFromEntry
+        );
+        Assert.assertEquals(expected, PasswordValidation.countFromList(passwordsToCheck));
     }
 }
