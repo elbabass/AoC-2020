3c8d9b9d64ccfa4e398f184dac9d4fea9207f5c8
Bastien Gallay <bgallay@palo-it.com>
2021-01-02 12:53:56 +0100

    Refactoring to simplify tests

--- src/main/java/org/util/AoC2020/D07/ShinyBagContainer.java
+++ src/main/java/org/util/AoC2020/D07/ShinyBagContainer.java
@@ -2,6 +2,6 @@ package org.util.AoC2020.D07;
 
 public class ShinyBagContainer {
     public static long countShinyBagsContainers(String entry) {
-        return entry.contains("shiny bag")?1:0;
+        return entry.contains("shiny bag") ? 1 : 0;
     }
 }
--- src/test/java/D07/ShinyBagContainerTest.java
+++ src/test/java/D07/ShinyBagContainerTest.java
@@ -7,54 +7,42 @@ import org.util.AoC2020.D07.ShinyBagContainer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ShinyBagContainerTest {
+    private final String[] shades = {"light",
+            "dark",
+            "bright",
+            "muted",
+            "shiny",
+            "dark",
+            "vibrant",
+            "faded",
+            "dotted"};
+    private final String[] colors = {"red",
+            "orange",
+            "white",
+            "yellow",
+            "gold",
+            "olive",
+            "plum",
+            "blue",
+            "black"};
+
     @Provide
     Arbitrary<String> shades() {
-        return Arbitraries.of(
-                "light",
-                "dark",
-                "bright",
-                "muted",
-                "shiny",
-                "dark",
-                "vibrant",
-                "faded",
-                "dotted"
-        );
+        return Arbitraries.of(shades);
     }
 
     @Provide
     Arbitrary<String> color() {
-        return Arbitraries.of(
-                "red",
-                "orange",
-                "white",
-                "yellow",
-                "gold",
-                "olive",
-                "plum",
-                "blue",
-                "black"
-        );
+        return Arbitraries.of(colors);
     }
 
     @Property
     void canCountBagContainingDirectlyShinyBags(
             @ForAll("shades") String shade,
             @ForAll("color") String color,
-            @ForAll @IntRange(min = 2, max = 9) Integer number
-            ){
-        String entry = shade + " " + color + " contain " + number + " shiny bags";
-        long expected = 1L;
-        long actual = ShinyBagContainer.countShinyBagsContainers(entry);
-        assertThat(expected).isEqualTo(actual);
-    }
-
-    @Property
-    void canCountBagContainingDirectly1ShinyBag(
-            @ForAll("shades") String shade,
-            @ForAll("color") String color
-    ){
-        String entry = shade + " " + color + " contain 1 shiny bag";
+            @ForAll @IntRange(min = 1, max = 9) Integer number
+    ) {
+        String entry = getSingleEntry(shade, color, number, " shiny bag");
         long expected = 1L;
         long actual = ShinyBagContainer.countShinyBagsContainers(entry);
         assertThat(expected).isEqualTo(actual);
@@ -64,12 +52,16 @@ public class ShinyBagContainerTest {
     void canCountBagContainingNoShinyBag(
             @ForAll("shades") String shade,
             @ForAll("color") String color,
-            @ForAll @IntRange(min = 2, max = 9) Integer number
-    ){
-        String entry = shade + " " + color + " contain " + number + " blue bags";
+            @ForAll @IntRange(min = 1, max = 9) Integer number
+    ) {
+        String entry = getSingleEntry(shade, color, number, " blue bag");
         long expected = 0L;
         long actual = ShinyBagContainer.countShinyBagsContainers(entry);
         assertThat(expected).isEqualTo(actual);
+    }
+
+    private String getSingleEntry(@ForAll("shades") String shade, @ForAll("color") String color, @ForAll @IntRange(min = 2, max = 9) Integer number, String innerBag) {
+        return shade + " " + color + " contain " + number + innerBag + ((number > 1) ? "s" : "");
     }
 
 }
