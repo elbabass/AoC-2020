a782a8be9688b3240f495956decff811d2b75c08
Bastien Gallay <bgallay@palo-it.com>
2021-01-02 12:16:05 +0100

    Day 7 initialization

---
+++ src/main/java/org/util/AoC2020/D07/Day07Main.java
@@ -0,0 +1,9 @@
+package org.util.AoC2020.D07;
+
+import org.util.AoC2020.AdventOfCodeMain;
+
+public class Day07Main extends AdventOfCodeMain {
+    public static void main(String[] args) {
+
+    }
+}
