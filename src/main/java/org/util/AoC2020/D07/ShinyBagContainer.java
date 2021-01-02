a4b92effc91a79a2892d6876518a9dea59e81bfd
Bastien Gallay <bgallay@palo-it.com>
2021-01-02 17:01:44 +0100

    Enable single container detection

--- src/main/java/org/util/AoC2020/D07/ShinyBagContainer.java
+++ src/main/java/org/util/AoC2020/D07/ShinyBagContainer.java
@@ -1,7 +1,53 @@
 package org.util.AoC2020.D07;
 
+import org.javatuples.Pair;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ShinyBagContainer {
-    public static long countShinyBagsContainers(String entry) {
-        return entry.contains("shiny bag") ? 1 : 0;
+    private final List<String> entries;
+    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+    private final Map<String, CanContainBag> containers;
+    private final Pattern pattern = Pattern.compile("(\\w+ \\w+) bags contain (\\d) ([\\w ]+) bag(s)?");
+
+    public ShinyBagContainer(List<String> entries) {
+        this.entries = entries;
+        containers = new HashMap<>() {{
+            entries.stream()
+                    .map(entry -> getCanContainBagPair(entry))
+                    .filter(result -> result != null && this.put(result.getValue0(), result.getValue1()) != null)
+                    .map(result -> "Duplicate key : old = " + this.get(result.getValue0()) + ", new = " + result.getValue1()).forEach(System.out::println);
+
+        }};
     }
+
+    public long countShinyBagsContainers() {
+        return entries.stream().filter(this::containsShinyBag).count();
+    }
+
+    private Pair<String, CanContainBag> getCanContainBagPair(String entry) {
+        Matcher matcher = pattern.matcher(entry);
+        if (matcher.matches()) {
+            CanContainBag canContainBag = new CanContainBag(matcher.group(3), Long.parseLong(matcher.group(2)));
+            return Pair.with(matcher.group(1), canContainBag);
+        }
+        return null;
+    }
+
+    private boolean containsShinyBag(String entry) {
+        boolean containerContains = false;
+        final Pair<String, CanContainBag> canContainBagPair = getCanContainBagPair(entry);
+        if (canContainBagPair != null && containers.containsKey(canContainBagPair.getValue0())) {
+            final String bagType = canContainBagPair.getValue1().bagType;
+            containerContains = containers.containsKey(bagType) && containers.get(bagType).bagType.contains("shiny");
+        }
+        return entry.contains("shiny bag")
+                || containerContains;
+    }
+
 }
--- src/test/java/D07/ShinyBagContainerTest.java
+++ src/test/java/D07/ShinyBagContainerTest.java
@@ -2,7 +2,12 @@ package D07;
 
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
+import net.jqwik.api.constraints.Size;
 import org.util.AoC2020.D07.ShinyBagContainer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,36 +37,67 @@ public class ShinyBagContainerTest {
     }
 
     @Provide
-    Arbitrary<String> color() {
+    Arbitrary<String> colors() {
         return Arbitraries.of(colors);
     }
 
     @Property
     void canCountBagContainingDirectlyShinyBags(
             @ForAll("shades") String shade,
-            @ForAll("color") String color,
+            @ForAll("colors") String color,
             @ForAll @IntRange(min = 1, max = 9) Integer number
     ) {
-        String entry = getSingleEntry(shade, color, number, " shiny bag");
+        String entry = getSingleEntry(shade, color, number, "shiny bag");
         long expected = 1L;
-        long actual = ShinyBagContainer.countShinyBagsContainers(entry);
+        long actual = new ShinyBagContainer(List.of(entry)).countShinyBagsContainers();
         assertThat(expected).isEqualTo(actual);
     }
 
     @Property
     void canCountBagContainingNoShinyBag(
             @ForAll("shades") String shade,
-            @ForAll("color") String color,
+            @ForAll("colors") String color,
             @ForAll @IntRange(min = 1, max = 9) Integer number
     ) {
-        String entry = getSingleEntry(shade, color, number, " blue bag");
+        String entry = getSingleEntry(shade, color, number, "blue bag");
         long expected = 0L;
-        long actual = ShinyBagContainer.countShinyBagsContainers(entry);
+        long actual = new ShinyBagContainer(List.of(entry)).countShinyBagsContainers();
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Property
+    void canCountSeveralLinesOfDirectShinyBags(
+            @ForAll @Size(value = 5) List<@From("shades") String> shades,
+            @ForAll @Size(value = 5) List<@From("colors") String> colors,
+            @ForAll @Size(value = 5) List<@IntRange(min = 1, max = 9) Integer> numbers
+    ) {
+        List<String> entries = IntStream
+                .range(0, shades.size())
+                .mapToObj(i -> getSingleEntry(shades.get(i), colors.get(i), numbers.get(i), "shiny bags"))
+                .collect(Collectors.toList());
+        long expected = 5L;
+        long actual = new ShinyBagContainer(entries).countShinyBagsContainers();
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    @Property
+    void canCountBagContainingShinyBagContainer(
+            @ForAll("shades") String shade,
+            @ForAll("colors") String color,
+            @ForAll @IntRange(min = 1, max = 9) Integer number
+    ) {
+        List<String> entries = List.of(
+                getSingleEntry(shade, color, number, "shiny bag"),
+                getSingleEntry("shadeOuter", "colorOuter", 1, shade + " " + color + " bag")
+        );
+
+        long expected = 2L;
+        long actual = new ShinyBagContainer(entries).countShinyBagsContainers();
         assertThat(expected).isEqualTo(actual);
     }
 
     private String getSingleEntry(@ForAll("shades") String shade, @ForAll("color") String color, @ForAll @IntRange(min = 2, max = 9) Integer number, String innerBag) {
-        return shade + " " + color + " contain " + number + innerBag + ((number > 1) ? "s" : "");
+        return shade + " " + color + " bags contain " + number + " " + innerBag + ((number > 1) ? "s" : "");
     }
 
 }
